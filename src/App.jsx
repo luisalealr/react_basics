502a483101ee74f09848ae9f71fba3f16607b80a
luisalealr <luisafernandalero@gmail.com>
2025-07-31 20:56:22 -0500

    adding logic in card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 import Card from "./components/Card";
+import style from "./App.module.css";
 
 function App() {
+  const estudiantes = [
+    {
+      name: "Luisa Leal",
+      rol: "Estudiante",
+      imagen: "https://i.pravatar.cc/150",
+    },
+    {
+      name: "Andersson Cardenas",
+      rol: "Estudiante",
+      imagen: "https://i.pravatar.cc/150",
+    },
+    {
+      name: "Welinton Suarez",
+      rol: "Profesor",
+      imagen: "https://i.pravatar.cc/150",
+    },
+  ];
+
   return (
     <>
-      <Card
-        name="Luisa Leal"
-        rol="Estudiante"
-        imagen="https://i.pravatar.cc/150"
-      />
-      <Card
-        name="Andersson Cardenas"
-        rol="Estudiante"
-        imagen="https://i.pravatar.cc/150"
-      />
-      <Card
-        name="Welinton Suarez"
-        rol="Profesor"
-        imagen="https://i.pravatar.cc/150"
-      />
+      <h1 className={style.title}>Lista de estudiantes</h1>
+      <section className={style.sectionCards}>
+        {estudiantes.map((estudiante, index) => (
+          <Card
+            key={index}
+            name={estudiante.name}
+            rol={estudiante.rol}
+            imagen={estudiante.imagen}
+          />
+        ))}
+      </section>
     </>
   );
 }
